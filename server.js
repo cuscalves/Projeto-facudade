6b1f243fab9369fae4e30c5949bd5c56dc2c992f
Caethano1992 <caethano92@gmail.com>
2024-06-16 21:15:38 -0300

    ajustes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 8080;
+const port = 80;
 
 const bodyParser = require('body-parser');
 
